4986d4422b0361587fad378286e90579eea7aa1d
Thomas <thomas@sctr.net>
2024-12-24 00:04:16 -0600

    optimized code a bit

--- src/Modifiers/PlaceModifier.php
+++ src/Modifiers/PlaceModifier.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Intervention\Image\Drivers\Vips\Modifiers;
 
 use Intervention\Image\Interfaces\ImageInterface;
+use Intervention\Image\Interfaces\PointInterface;
 use Intervention\Image\Interfaces\SpecializedInterface;
 use Intervention\Image\Modifiers\PlaceModifier as GenericPlaceModifier;
 use Jcupitt\Vips\BlendMode;
@@ -15,56 +16,60 @@ class PlaceModifier extends GenericPlaceModifier implements SpecializedInterface
     public function apply(ImageInterface $image): ImageInterface
     {
         $watermark = $this->driver()->handleInput($this->element);
+        $watermarkNative = $watermark->core()->native();
         $position = $this->getPosition($image, $watermark);
-        $imageSize = $image->size()->align($this->position);
 
         if ($this->opacity < 100) {
-            if (!$watermark->core()->native()->hasAlpha()) {
-                $watermark->core()->setNative(
-                    $watermark->core()->native()->bandjoin_const(255)
-                );
+            if (!$watermarkNative->hasAlpha()) {
+                $watermarkNative = $watermarkNative->bandjoin_const(255);
             }
 
-            $watermark->core()->setNative(
-                $watermark->core()->native()->multiply([
-                    1.0,
-                    1.0,
-                    1.0,
-                    $this->opacity / 100,
-                ])
-            );
+            $watermarkNative = $watermarkNative->multiply([
+                1.0,
+                1.0,
+                1.0,
+                $this->opacity / 100,
+            ]);
         }
 
-        if ($watermark->core()->native()->hasAlpha()) {
-            $watermark->core()->setNative(
-                $watermark->core()->native()->embed(
-                    $position->x(),
-                    $position->y(),
-                    $imageSize->width(),
-                    $imageSize->height(),
-                    [
-                        'extend' => Extend::BACKGROUND,
-                        'background' => [0, 0, 0, 0],
-                    ]
-                )
+        $this->placeWatermark($watermarkNative, $position, $image);
+
+        return $image;
+    }
+
+    private function placeWatermark(
+        mixed $watermarkNative,
+        PointInterface $position,
+        ImageInterface $image
+    ): void {
+        if ($watermarkNative->hasAlpha()) {
+            $imageSize = $image->size()->align($this->position);
+
+            $watermarkNative = $watermarkNative->embed(
+                $position->x(),
+                $position->y(),
+                $imageSize->width(),
+                $imageSize->height(),
+                [
+                    'extend' => Extend::BACKGROUND,
+                    'background' => [0, 0, 0, 0],
+                ]
             );
 
             $image->core()->setNative(
                 $image->core()->native()->composite2(
-                    $watermark->core()->native(),
+                    $watermarkNative,
                     BlendMode::OVER
                 )
             );
         } else {
             $image->core()->setNative(
                 $image->core()->native()->insert(
-                    $watermark->core()->native()->bandjoin_const(255),
+                    $watermarkNative->bandjoin_const(255),
                     $position->x(),
                     $position->y()
                 )
             );
         }
-
-        return $image;
     }
 }
