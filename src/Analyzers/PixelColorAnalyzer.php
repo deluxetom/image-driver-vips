34b4f8f36d8196f17716c98bf2119ca785ba6592
Thomas <thomas@sctr.net>
2024-12-24 10:14:30 -0600

    brightness modifier

--- src/Analyzers/PixelColorAnalyzer.php
+++ src/Analyzers/PixelColorAnalyzer.php
@@ -9,6 +9,7 @@ use Intervention\Image\Interfaces\ColorInterface;
 use Intervention\Image\Interfaces\ColorspaceInterface;
 use Intervention\Image\Interfaces\ImageInterface;
 use Intervention\Image\Interfaces\SpecializedInterface;
+use Jcupitt\Vips\BandFormat;
 use Jcupitt\Vips\Image as VipsImage;
 
 class PixelColorAnalyzer extends GenericPixelColorAnalyzer implements SpecializedInterface
@@ -26,10 +27,23 @@ class PixelColorAnalyzer extends GenericPixelColorAnalyzer implements Specialize
      */
     protected function colorAt(ColorspaceInterface $colorspace, VipsImage $vipsImage): ColorInterface
     {
+        $normalizer = function ($value) use ($vipsImage): int {
+            if ($vipsImage->format === BandFormat::UCHAR) {
+                return (int) $value;
+            }
+
+            // Normalize for other formats (e.g., 16-bit images or float)
+            $maxValue = 255; // Max for 8-bit images
+            $minValue = $vipsImage->min();
+            $range = $vipsImage->max() - $minValue;
+
+            return (int) round((($value - $minValue) / $range) * $maxValue);
+        };
+
         return $this->driver()
             ->colorProcessor($colorspace)
             ->nativeToColor(array_map(
-                fn ($value): int => (int) $value,
+                $normalizer,
                 $vipsImage->getpoint($this->x, $this->y)
             ));
     }
