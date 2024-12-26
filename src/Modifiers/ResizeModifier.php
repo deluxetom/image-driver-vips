6ac25f8f06aab208617798b61fa45cb6f3d0fc47
Thomas <thomas@sctr.net>
2024-12-26 15:37:32 -0600

    added test

--- src/Modifiers/ResizeModifier.php
+++ src/Modifiers/ResizeModifier.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace Intervention\Image\Drivers\Vips\Modifiers;
 
+use Intervention\Image\Exceptions\GeometryException;
+use Intervention\Image\Exceptions\RuntimeException;
 use Intervention\Image\Interfaces\ImageInterface;
+use Intervention\Image\Interfaces\SizeInterface;
 use Intervention\Image\Interfaces\SpecializedInterface;
 use Intervention\Image\Modifiers\ResizeModifier as GenericResizeModifier;
 
@@ -17,14 +20,29 @@ class ResizeModifier extends GenericResizeModifier implements SpecializedInterfa
      */
     public function apply(ImageInterface $image): ImageInterface
     {
+        $resizeTo = $this->getAdjustedSize($image);
+
         $image->core()->setNative(
-            $image->core()->native()->thumbnail_image($this->width, [
-                'height' => $this->height,
+            $image->core()->native()->thumbnail_image($resizeTo->width(), [
+                'height' => $resizeTo->height(),
                 'size' => 'force',
                 'no_rotate' => true,
             ])
         );
 
         return $image;
+    }
+
+    /**
+     * Return the size the modifier will resize to
+     *
+     * @param ImageInterface $image
+     * @throws RuntimeException
+     * @throws GeometryException
+     * @return SizeInterface
+     */
+    protected function getAdjustedSize(ImageInterface $image): SizeInterface
+    {
+        return $image->size()->resize($this->width, $this->height);
     }
 }
