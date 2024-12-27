ffbb700417a5c93483b374a64a7ba1d731d55352
Oliver Vogel <oliver@olivervogel.com>
2024-12-27 11:12:42 +0100

    Remove write calls for test files

--- tests/Unit/Modifiers/BrightnessModifierTest.php
+++ tests/Unit/Modifiers/BrightnessModifierTest.php
@@ -15,10 +15,8 @@ final class BrightnessModifierTest extends BaseTestCase
     public function testApply(): void
     {
         $image = $this->readTestImage('trim.png');
-        $image->save('brightness1.png');
         $this->assertEquals('00aef0', $image->pickColor(14, 14)->toHex());
         $image->modify(new BrightnessModifier(30));
-        $image->save('brightness2.png');
         $this->assertEquals('1eccff', $image->pickColor(14, 14)->toHex());
     }
 }
